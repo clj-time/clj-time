d412423a5c4cc40d75425f4512176225085f0812
Christopher Mark Gore <cgore@cgore.com>
2016-10-11 16:50:42 -0500

    Adding noon functions.

--- test/clj_time/core_test.clj
+++ test/clj_time/core_test.clj
@@ -74,6 +74,26 @@
     (is (= 0    (second d)))
     (is (= 0    (milli  d)))))
 
+(deftest test-date-noon-and-accessors
+  (let [d (date-noon 1986)]
+    (is (= 1986 (year   d)))
+    (is (= 1    (month  d)))
+    (is (= 1    (day    d)))
+    (is (= 12   (hour   d)))
+    (is (= 0    (minute d)))
+    (is (= 0    (second d)))
+    (is (= 0    (milli  d)))))
+
+(deftest test-local-date-noon-and-accessors
+  (let [d (local-date-noon 1986)]
+    (is (= 1986 (year   d)))
+    (is (= 1    (month  d)))
+    (is (= 1    (day    d)))
+    (is (= 12   (hour   d)))
+    (is (= 0    (minute d)))
+    (is (= 0    (second d)))
+    (is (= 0    (milli  d)))))
+
 (deftest test-min-and-max
   (let [ds [(date-time 1986 10 14 4 3 2 1)
             (date-time 1986 12 14 4 3 2 1)
