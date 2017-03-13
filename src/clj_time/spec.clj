a3b2b6c262ce34a64bdb762ad934ca2ea42210f8
Darrick Wiebe <darrickw@gmail.com>
2017-03-13 01:08:14 -0400

    Add some simple tests for the spec namespace

--- src/clj_time/spec.clj
+++ src/clj_time/spec.clj
@@ -13,7 +13,10 @@
   [x]
   (instance? BaseDateTime x))
 
-(defn utc-date-time? [x]
+(defn
+  ^{::spec/name "clj-time.spec/utc-date-time?"}
+  utc-date-time?
+  [x]
   (and (date-time? x)
        (= (.getZone ^BaseDateTime x) DateTimeZone/UTC)))
 
@@ -39,27 +42,22 @@
   (gen/one-of [(gen/return DateTimeZone/UTC)
                (spec/gen @all-time-zones)]))
 
-(spec/def ::modern-era (spec/int-in (to-long (date-time 2006 3 24 14 49 31))
-                                    (to-long (date-time 2017 1 20 16 00 00))))
+(spec/def ::past (spec/int-in (to-long (date-time 2001 1 1 00 00 00))
+                              (to-long (date-time 2010 12 31 00 00 00))))
 
-(spec/def ::questionable-future (spec/int-in (to-long (date-time 2017 1 20 16 00 00))
-                                             (to-long (date-time 2030 12 31 23 59 59))))
+(spec/def ::past-and-future (spec/int-in (to-long (date-time 2011 1 1 00 00 00))
+                                         (to-long (date-time 2030 12 31 23 59 59))))
+
+(spec/def ::future (spec/int-in (to-long (date-time 2031 1 1 0 00 00))
+                                (to-long (date-time 2040 12 31 23 59 59))))
 
 (defn ^:dynamic *period*
   "Dynamically bind this to choose the range of your generated dates."
   []
-  (gen/one-of [(spec/gen ::modern-era) (spec/gen ::questionable-future)]))
+  (spec/gen ::past-and-future))
 
 (spec/def ::time-zone
           (spec/with-gen time-zone? *time-zones*))
-
-; FIXME: despite specifying a time zone, this seems to always produce UTC. I don't know what I'm doing wrong.
-(spec/def ::date-time
-          (spec/with-gen utc-date-time?
-                         #(gen/fmap (fn [[ms tz]]
-                                      (DateTime. ms ^DateTimeZone tz))
-                                    (gen/tuple (*period*)
-                                               (*time-zones*)))))
 
 (spec/def ::utc-date-time
           (spec/with-gen utc-date-time?
@@ -75,13 +73,3 @@
           (spec/with-gen local-date-time?
                          #(gen/fmap (fn [ms] (LocalDateTime. ms))
                                     (*period*))))
-
-
-(comment
-  (gen/sample (spec/gen ::utc-date-time))
-  (gen/sample (spec/gen ::local-date))
-  (gen/sample (spec/gen ::local-date-time))
-  (binding [*period* #(spec/gen int?)]
-    (gen/sample (spec/gen ::local-date)))
-  (gen/sample (spec/gen ::time-zone))
-  (map #(.getZone ^DateTime %) (gen/sample (spec/gen ::date-time))))
